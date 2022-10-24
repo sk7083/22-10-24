ed94dbce26fec69733da3b00c39ea4d8914496cb
SunWoo <tjsdn7083@naver.com>
2022-10-24 21:37:42 +0900

    자바 01일차 : 변수 선언 예제

---
+++ java_ssw/src/day01/VariableEx01.java
@@ -0,0 +1,30 @@
+package day01;
+
+public class VariableEx01 {
+
+	public static void main(String[] args) {
+
+		boolean isEven = false;
+		char operator = '+';
+		int age = 30;
+		float pi1 = 3.1415f;	//float은 접미사 F나 f를 반드시 붙여야한다.
+		double pi2 = 3.1415;
+		//참조형 문자열 String
+		String str = "+";
+		
+		//사과의 개수를 저장하기 위한 변수를 선언햅세요.
+		
+		int apple;
+		
+		//학생 평균 성적을 저장하기 위한 변수를 선언해보세요.
+		
+		double gradeAvg;
+		
+		/* 관례 : 규칙은 아니지만 개발자들 간에 관례상 룰
+		 *  - 변수는 소문자로 시작
+		 *  - 카멜 표기법(낙타 표기법)을 따라서 두번째 글자부터 첫글자는 대문자로 표기
+		 *  - 클래스와 인터페이스는 대문자로 시작
+		 */
+	}
+
+}
