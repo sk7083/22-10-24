179b562b4ddc3439425a396270b7c2777aba022f
SunWoo <tjsdn7083@naver.com>
2022-10-24 21:37:28 +0900

    자바 01일차 : Hellow world 기본 예제

---
+++ java_ssw/src/day01/HelloWorld.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class HelloWorld {
+	// 프로그램이 시작하려면 main문이 잇어야 한다.
+	public static void main(String[] args) {
+		// 는 한줄 주석
+		/* 이 사이엔 모든 줄을 주석 처리함 */
+		/**
+		 * 여러줄 주석, 기능(메소드)을 설명할 때 활용
+		 */
+		System.out.println("Hello world!"); // 콘솔에 원하는 값을 출력하는 기능
+		
+	}
+}
