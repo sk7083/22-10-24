060cf9f209a482e514f0c0c0b534ba0ff501e874
SunWoo <tjsdn7083@naver.com>
2022-10-25 18:09:15 +0900

    자바 02일차 : 형변환 예제

---
+++ java_ssw/src/day02/TypeCatingEx01.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class TypeCatingEx01 {
+
+	public static void main(String[] args) {
+		//접미사 f(F)가 없는 실수 리터럴은 double형
+		//1.23은 8byte인 double이고, num1은 4byte인 float이기 때문에
+		//자동 형변환 불가능.
+		float num1 = (float)1.23;
+		
+		
+	}
+
+}
