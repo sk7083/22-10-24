b4b61191166c23858f6234c650a192bc10aed3bd
SunWoo <tjsdn7083@naver.com>
2022-11-08 21:47:26 +0900

    메모장

--- java_ssw/src/testhooom/Sww.java
+++ java_ssw/src/testhooom/Sww.java
@@ -2,10 +2,22 @@ package testhooom;
 
 public class Sww {
 	public static void main(String[] args) {
-		System.out.println("asdasdsad");
-		System.out.println("qqqqq");
-	}
-	
+		
+		for (int i =1; i<=20; i++) {
 
-	
+			switch(i % 5) {
+
+			case 0 :
+
+			System.out.println(i + " : 5의 배수입니다.");
+
+			break;
+
+			default :
+				System.out.println(i + " : 5의 배수가 아닙니다.");
+		
+			}
+
+			}
+	}
 }
