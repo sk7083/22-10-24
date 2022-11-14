4cdaa798f1b9740eca36a266cff52f54f07c4e81
SunWoo <tjsdn7083@naver.com>
2022-11-14 21:46:07 +0900

    자바 16일차 : AccountBook 예제

---
+++ java_ssw/src/example/accountbook/AccountServiceImp.java
@@ -0,0 +1,73 @@
+package example.accountbook;
+
+import java.util.ArrayList;
+
+public class AccountServiceImp implements AccountService{
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
+
+	@Override
+	public void insertItem(ArrayList<Item> list, Item item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printItem(ArrayList<Item> list) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean updateItem(ArrayList<Item> list, int index, Item item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteItem(ArrayList<Item> list, int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void printMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void runMenu(ArrayList<Item> list, int menu) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
