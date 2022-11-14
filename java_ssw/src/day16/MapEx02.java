479b9ece004969ad6f262ee0e0383147f1a9ef3e
SunWoo <tjsdn7083@naver.com>
2022-11-14 21:45:05 +0900

    자바 16일차 : Map 예제

---
+++ java_ssw/src/day16/MapEx02.java
@@ -0,0 +1,29 @@
+package day16;
+
+import java.util.HashMap;
+
+import lombok.Data;
+
+public class MapEx02 {
+
+	public static void main(String[] args) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("user", new User("abc", "def"));
+		map.put("age", 22);
+		map.put("address", "서울시");
+		System.out.println(map.get("user"));
+		System.out.println(map.get("age"));
+		System.out.println(map.get("address"));
+		System.out.println(map.get("123"));	//key에 맞는 value가 없으면 null이 출력
+		
+	}
+
+}
+@Data
+class User {
+	private String id, pw;
+	public User(String id, String pw) {
+		this.id = id;
+		this.pw = pw;
+	}
+}
