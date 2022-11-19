55372b340f5b5fe886982b9f5861d53acb452a28
jaewan <t1653tt@gmail.com>
2022-11-19 20:00:35 +0900

    선우 마트 문제.

---
+++ java_ssw/src/studyTest/Main.java
@@ -0,0 +1,25 @@
+package studyTest;
+
+public class Main {
+	// 선우 마트
+	//ArrayList <Member> swMart = new ArrayLisy<Member>(); 에 회원 관련된 정보들은 전부 저장.
+	
+	
+	//1.마트 회원가입 (member Class 사용)
+	
+	
+	//2.마트 회원 조회 (Mart Class 사용)
+	
+	
+	//3.마트 회원 검색 후 회원정보 수정(Mart Class 사용)
+	
+	
+	//4.마트 회원에 구매한 일별 영수증 입력 기능(Mart Class 사용, 영수증 정보 보관은 Map을 사용)
+	
+	
+	//5.구매한 영수증 등록 시 총액의 2% 만큼 회원 정보 Point에 추가.(Mart Class 사용, 영수증 정보 보관은 Map을 사용)
+	
+	
+	//6.마트 포인트 랭킹 출력.(Mart Class 사용, 출력은 등수/이름/포인트점수 ) 
+
+}
