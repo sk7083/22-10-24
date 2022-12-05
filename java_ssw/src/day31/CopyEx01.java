4882ef31dc550bf1afad14134d0d9bf38e7c4bdf
SunWoo <tjsdn7083@naver.com>
2022-12-05 21:39:31 +0900

    자바 32일 차 : Copy 예제

---
+++ java_ssw/src/day31/CopyEx01.java
@@ -0,0 +1,62 @@
+package day31;
+
+import lombok.Data;
+
+public class CopyEx01 {
+	public static void main(String[] args) {
+		
+		//얕은 복사
+		Test t1 = new Test(10, 20, "홍길동");
+		Test t2 = t1;
+		System.out.println("t1 : " + t1);
+		System.out.println("t2 : " + t2);
+		
+		System.out.println("============================");
+		t1.setNum1(30);
+		t1.getSub();
+		System.out.println("t1 : " + t1);
+		System.out.println("t2 : " + t2);
+		
+		//깊은 복사
+		Test t3 = new Test(10,20, "홍길동");
+		Test t4 = new Test(t3);
+		System.out.println("============================");
+		System.out.println("t3 : " + t3);
+		System.out.println("t4 : " + t4);
+		
+		System.out.println("============================");
+		t3.setNum1(30);
+		System.out.println("t3 : " + t3);
+		System.out.println("t4 : " + t4);
+	}
+}
+
+@Data
+class Test {
+	int num1, num2;
+	Sub sub;
+	
+	public Test(int num1, int num2, String name) {
+		this.num1 = num1;
+		this.num2 = num2;
+		this.sub = new Sub(name);
+	}
+
+	public Test(Test t) {
+		this.num1 = t.num1;
+		this.num2 = t.num2;
+		this.sub = new Sub(t.sub);
+	}
+	
+}
+
+class Sub{
+	String name;
+
+	public Sub(String name) {
+		this.name = name;
+	}
+	public Sub(Sub sub) {
+		this.name = sub.name;
+	}
+}
