699d4f4763d1a6f97aebdde129fa7ad88f4dbce3
SunWoo <tjsdn7083@naver.com>
2022-12-06 21:41:38 +0900

    자바 32일차 : Wrapper 예제

---
+++ java_ssw/src/day32/WrapperEx01.java
@@ -0,0 +1,23 @@
+package day32;
+
+public class WrapperEx01 {
+	public static void main(String[] args) {
+		String str = "123";
+		
+		//문자열을 정수로
+		int num = Integer.parseInt(str);
+		System.out.println(num);
+		
+		//문자열을 실수로
+		str = "3.14";
+		double dnum = Double.parseDouble(str);
+		System.out.println(dnum);
+		
+		//정수를 문자열로
+		num = 30;
+		//str = "" + num; 이렇게 변환도 가능
+		str = String.valueOf(num);
+		System.out.println(str);
+		
+	}
+}
