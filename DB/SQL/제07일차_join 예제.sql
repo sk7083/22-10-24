a4483b256c8b6afa4fa10663444de1d44a0a20fa
SunWoo <tjsdn7083@naver.com>
2022-12-15 21:41:51 +0900

    데이터베이스 07일차 : join예제

---
+++ DB/SQL/제07일차_join 예제.sql
@@ -0,0 +1,57 @@
+-- 의류 제품들을 높은 가격순으로 조회
+select mc_name as 분류, pr_title as 제품명, pr_price as 가격 from product
+	join sub_category on pr_sc_num = sc_num
+    join main_category on mc_num = sc_mc_num
+    where mc_name = '의류'
+    order by pr_price desc;
+    
+-- 의류 제품들을 낮은 가격순으로 조회
+select mc_name as 분류, pr_title as 제품명, pr_price as 가격 from product
+	join sub_category on pr_sc_num = sc_num
+    join main_category on mc_num = sc_mc_num
+    where mc_name = '의류'
+    order by pr_price asc;
+-- 위 코드와 동일한 결과지만 좀더 속도가 빠르며 효율적임
+select mc_name as 분류, pr_title as 제품명, pr_price as 가격 from product
+	join sub_category on pr_sc_num = sc_num
+    join (select * from main_category where mc_name = '의류') as mc on mc_num = sc_mc_num
+    order by pr_price asc;
+
+set sql_mode =(select replace(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+
+-- 제품별 제품명, 판매량을 조회
+select pr_title as 제품명, ifnull(sum(ol_amount),0) as 판매량 from order_list
+	right join product_option on po_num = ol_po_num
+    right join product on po_pr_num = pr_num
+    group by po_pr_num
+    order by 판매량 desc;
+
+
+-- 의류 제품중 제품별 판매량을 조회, 제품명, 판매량을 조회
+select mc_name as 분류, pr_title as 제품명, ifnull(sum(ol_amount),0) as 판매량 from order_list
+	right join product_option on po_num = ol_po_num
+    right join product on po_pr_num = pr_num
+    join sub_category on sc_num = pr_sc_num
+    join main_category on mc_num = sc_mc_num
+    where mc_name = '의류'
+    group by po_pr_num
+    order by 판매량 desc;
+
+-- 제품별 최신순으로 조회
+select * from product
+	order by pr_reg_date desc;
+
+-- 제품별 최신순으로 조회, 60개씩 보기
+select * from product
+	order by pr_reg_date desc
+    limit 60; -- limit 0, 60도 맞는 표현
+    
+    
+    
+    
+    
+    
+    
+    
+
+
