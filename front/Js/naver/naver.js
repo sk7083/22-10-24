96af278d578cc166e366b5c23cc32c0133b624d2
SunWoo <tjsdn7083@naver.com>
2023-02-01 21:40:28 +0900

    js naver 예제

---
+++ front/Js/naver/naver.js
@@ -0,0 +1,108 @@
+/* 롤링 관련 이벤트 */
+$(function(){
+	$('.box-body-right2 .btn-next').click(function(e){
+		e.preventDefault();
+		moveLeft(liRight2, ulRight2, timeRight2);
+	});
+	
+	$('.box-body-right2 .btn-prev').click(function(e){
+		e.preventDefault();
+		moveRight(liRight2, ulRight2, timeRight2);
+	});
+	
+	$('.box-body-right2').hover(function(){
+		clearInterval(rollingRight2);
+	}, function(){
+		rollingRight2 = rollingLeft(rollingObjRigh2.li, rollingObjRigh2.ul, rollingObjRigh2.time, rollingObjRigh2.duration);
+	});
+	
+	$('.box-body-left2 .list-issue').hover(function(){
+		clearInterval(rollingLeft2);
+	}, function(){
+		rollingLeft2 = rollingTop(rollingObjLeft2);
+	});
+	
+});
+//페이지네이션 버튼 이벤트
+$(function(){
+	$('.box-shop-in .btn-prev').click(function(e){
+		e.preventDefault();
+		let boxShopIn = $(this).parents('.box-shop-in');
+		let curPage = boxShopIn.find('.current-page').first().text();
+		let maxPage = boxShopIn.find('.max-page').first().text();
+		curPage = +curPage;
+		curPage = --curPage == 0 ? maxPage : curPage;
+		boxShopIn.find('.current-page').text(curPage);
+	});
+	$('.box-shop-in .btn-next').click(function(e){
+		e.preventDefault();
+		let boxShopIn = $(this).parents('.box-shop-in');
+		let curPage = boxShopIn.find('.current-page').first().text();
+		let maxPage = boxShopIn.find('.max-page').first().text();
+		curPage = +curPage;
+		curPage = ++curPage > maxPage ? 1 : curPage;
+		boxShopIn.find('.current-page').text(curPage);
+	});
+});
+
+
+let liRight2 = '.box-body-right2 .item-stock';
+let ulRight2 = '.box-body-right2 .list-stock';
+let timeRight2 = 1000;
+let durationRight2 = 2000;
+
+let rollingObjRigh2 = {
+	li : '.box-body-right2 .item-stock',
+ 	ul : '.box-body-right2 .list-stock',
+	time : 1000,
+	duration : 2000
+}
+let rollingObjLeft2 = {
+	liSelector : '.box-body-left2 .item-issue',
+	ulSelector : '.box-body-left2 .list-issue',
+	duration : 2000,
+	animationTime : 1000
+}
+let rollingRight2 = rollingLeft(rollingObjRigh2.li, rollingObjRigh2.ul, rollingObjRigh2.time, rollingObjRigh2.duration);
+let rollingLeft2 = rollingTop(rollingObjLeft2);
+
+function moveLeft(liSelector, ulSelector, time){
+	let width = $(liSelector).first().innerWidth();
+	if(!$(liSelector).first().is(':animated')){
+		$(liSelector).first().animate({
+			marginLeft : `-${width}px`
+		},time, function(){
+			$(this).detach().appendTo(ulSelector).removeAttr('style');
+		});
+	}
+}
+function moveRight(liSelector, ulSelector, time){
+	let width = $(liSelector).first().innerWidth();
+	if(!$(liSelector).first().is(':animated')){
+		$(liSelector)
+			.last()
+			.detach()
+			.prependTo(ulSelector)
+			.css('marginLeft',`-${width}px`)
+			.animate({
+				marginLeft : 0
+			}, time);
+	}
+}
+function rollingLeft(liSelector, ulSelector, animationTime, duration){
+	return setInterval(moveLeft,duration,liSelector, ulSelector, animationTime);
+}
+function moveTop(liSelector, ulSelector, animationTime){
+	let height = $(liSelector).first().innerHeight();
+	if(!$(liSelector).first().is(':animated')){
+		$(liSelector).first().animate({
+				marginTop : `-${height}px`
+			}, animationTime, function(){
+				$(this).detach().appendTo(ulSelector).removeAttr('style');
+			});
+	}
+}
+function rollingTop(rollingObj){
+	return setInterval(moveTop,rollingObj.duration,rollingObj.liSelector, 
+		rollingObj.ulSelector, rollingObj.animationTime);
+}
