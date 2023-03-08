f927ddb7e5bdd8638f2d62058e8b2f9c8ed04855
SunWoo <tjsdn7083@naver.com>
2023-03-08 17:08:12 +0900

    스프링 test : 게시글 등록 (첨부파일) (복습)

--- spring project/springtest/src/main/java/kr/kh/test/service/BoardServiceImp.java
+++ spring project/springtest/src/main/java/kr/kh/test/service/BoardServiceImp.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import kr.kh.test.dao.BoardDAO;
+import kr.kh.test.utils.UploadFileUtils;
 import kr.kh.test.vo.BoardTypeVO;
 import kr.kh.test.vo.BoardVO;
+import kr.kh.test.vo.FileVO;
 import kr.kh.test.vo.MemberVO;
 
 @Service
@@ -16,6 +18,8 @@ public class BoardServiceImp implements BoardService{
 	@Autowired
 	BoardDAO boardDao;
 
+	String uploadPath = "D:\\uploadfiles";
+	
 	@Override
 	public ArrayList<BoardTypeVO> getBoardTypeList(MemberVO user) {
 		if(user == null || user.getMe_authority() == 0)
@@ -24,7 +28,7 @@ public class BoardServiceImp implements BoardService{
 	}
 
 	@Override
-	public boolean insertBoard(BoardVO board, MemberVO user, MultipartFile files) {
+	public boolean insertBoard(BoardVO board, MemberVO user, MultipartFile[] files) {
 		if(user == null || user.getMe_authority() == 0)
 			return false;
 		if(board == null || 
@@ -36,10 +40,28 @@ public class BoardServiceImp implements BoardService{
 		board.setBo_me_id(user.getMe_id());
 		
 		int isOk = boardDao.insertBoard(board);
-		
+
 		if(isOk == 0)
 			return false;
-		
+		//첨부파일 추가
+		if(files == null || files.length == 0)
+			return true;
+		for(MultipartFile file : files) {
+			if(file == null || file.getOriginalFilename().length() == 0)
+				continue;
+			try {
+				String path = UploadFileUtils.uploadFile(uploadPath, 
+						file.getOriginalFilename(), file.getBytes());
+				FileVO fileVo = new FileVO(board.getBo_num(), path, 
+						file.getOriginalFilename());
+				boardDao.insertFile(fileVo);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return true;
+			}
+		}
 		return true;
 	}
+	
+	
 }
